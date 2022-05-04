ce0b7d0d8273134b76fc884f1bd63f22585cae3b
Nishtha Das <72308276+n1shtha@users.noreply.github.com>
2022-05-04 20:55:36 +0530

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,11 +1,11 @@
 // JavaScript for enabling the map on load. Change the access token and the web page.
 
-mapboxgl.accessToken = 'pk.eyJ1Ijoiam9vc3RidXJnZXJzIiwiYSI6ImNsMXdiZWczdzBjN28zcG10MmJ0b3diem8ifQ.v5WXemHZmLCM2vEEZ6866g';
+mapboxgl.accessToken = 'pk.eyJ1IjoiaWxhbWFuaXNoIiwiYSI6ImNsMXQ4eDliMzB6N3kzb3FxbzY3ZmN5bDYifQ.swLACYH2fKaTe93_6hQ5sg';
 
 //Set the initial map view
 const map = new mapboxgl.Map({
   container: 'map', // container ID
-  style: 'mapbox://styles/joostburgers/cl1w8a046000q14rrrewcwdem', // style URL
+  style: 'mapbox://styles/ilamanish/cl2r6nsvb000c14p1x9wtvzwh', // style URL
   center: [77.0688997, 20.5272803], // starting position [lng, lat]
   zoom: 4, // starting zoom
   bearing: 0, //controls the left-right rotation of the map in degrees
